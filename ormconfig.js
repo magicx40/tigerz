ac0d100d54e5b639f4cf890777fc1f51aaeb63c1
hanzongyuan <hanzongyuan@foxmail.com>
2024-09-10 08:30:25 +0800

    feat: 添加config模块

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+export default {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'zhu88jie',
+    database: 'tigerz',
+    synchronize: false,
+    autoLoadEntities: true,
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './db/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import OrmConfig from '../ormconfig.js';
 
 @Module({
     imports: [
         UsersModule,
         AuthModule,
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'zhu88jie',
-            database: 'tigerz',
-            entities: [User],
-            synchronize: false,
+        ConfigModule.forRoot({
+            isGlobal: true,
         }),
+        TypeOrmModule.forRoot(OrmConfig),
     ],
     controllers: [AppController],
     providers: [AppService],
